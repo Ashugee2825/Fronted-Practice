ca73a86935d9d1d71ccbe0c8b3d0d023d4242bf1
Ashutosh Kumar <66332175+Ashugee2825@users.noreply.github.com>
2025-03-28 23:58:30 +0530

    Add files via upload

---
+++ kodnestfrontend/2ndclass.js
@@ -0,0 +1,19 @@
+<html>
+    <head> 
+        <title> </title>
+    </head>
+        <script>
+            {/* age = 25;
+            console.log("value of age is" ;age);
+            console.log(typeof age);
+            age = 'kodnest'
+            console.log('typeof age');
+            app = 'kodnest'
+ */}
+ var name ="pooja";
+{
+    console.log(name);
+}
+
+
+        </script>
